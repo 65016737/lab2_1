ab80d4ba3299abcad4aa5a8bb05f87440c08b581
65016737 <65016737@rbac.ac.th>
2025-11-07 10:22:06 +0700

    Anucha

--- menu.jsx
+++ menu.jsx
@@ -1,3 +1,11 @@
 console.log("HOME");
 console.log("PRODUCT");
 console.log("ABOUT");
+
+console.log("HOME");
+console.log("PRODUCT");
+console.log("ABOUT");
+
+console.log("HOME");
+console.log("PRODUCT");
+console.log("ABOUT");
